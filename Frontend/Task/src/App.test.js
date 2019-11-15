9e189bebf304c1bfba83ac6d1bceec3cc9285d85
theborgh <borghiborghi@gmail.com>
2019-11-15 23:48:35 +0100

    Add state management and testing dependencies, set up enzyme

--- Frontend/Task/src/App.test.js
+++ Frontend/Task/src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("is set up correctly", () => {
+  expect(typeof null).toBe("object");
+});
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
